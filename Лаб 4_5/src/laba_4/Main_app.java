e432f7a672a7b20642d82b5c7acee048819bbcdf
Asbarn <47718251+Asbarn@users.noreply.github.com>
2019-12-08 22:50:09 +0300

    Add files via upload

---
+++ Лаб 4_5/src/laba_4/Main_app.java
@@ -0,0 +1,18 @@
+package laba_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+import  java.io.IOException;
+public class Main_app {
+    public static void main(String[] args)
+    {
+
+
+        MainFrame frame=new MainFrame;
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+}
